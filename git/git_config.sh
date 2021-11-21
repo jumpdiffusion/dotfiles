33a8871b89de1c220dfacc7b82e929a5c0c86c6f
jumpdiffusion <jumpdiffusion@outlook.com>
2021-11-21 03:39:30 -0500

    add git config

---
+++ git/git_config.sh
@@ -0,0 +1,9 @@
+git config --global user.name "jumpdiffusion"
+git config --global user.email jumpdiffusion@outlook.com
+
+# set vim as text editor used by commands like git commit
+# git config --global core.editor vim
+# git config --global core.editor nvim
+
+# setup github username
+git config --global github.user "jumpdiffusion"
